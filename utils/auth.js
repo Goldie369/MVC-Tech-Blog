548b33d49bf2c2093ccabb0e697ff1be613657b8
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-04-30 13:07:19 -0500

    adding a comments for better understanding of the user

--- utils/auth.js
+++ utils/auth.js
@@ -1,9 +1,14 @@
+//-- Adding a function that checks whether a user is authenticated before allowing them to access certain routes in an Express.js application--//
+//-- The function takes three parameters: req (request), res (response), and next--//
+//-- The function checks whether the loggedIn property is set to true in the session object--//
+//-- If it is not, the user is redirected to the '/login' route using the res.redirect() method--//
+//-- the function is exported from the module using module.exports--//
 const withAuth = (req, res, next) => {
-  // If the user is not logged in, redirect the user to the login page
+  
   if (!req.session.loggedIn) {
     res.redirect('/login');
   } else {
-    // If the user is logged in, proceed with the next callback function
+    
     next();
   }
 };
