cf781e9d7e9603a1086276dd794be19654cf70fd
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-04-30 13:02:19 -0500

    adding comments for better understanding

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,15 @@
+//-- Importing the Sequelize library and the dotenv library--//
+//-- The sequelize variable is created by passing either the JAWSDB_URL or the local MySQL database--//
+//-- he constructor creates a connection to the local database using the dialect 'mysql' and the provided credentials--//
+//-- the sequelize variable is exported from the module, allowing it to be used by other modules in the application for querying the database--//
+
+
 const Sequelize = require('sequelize');
-require('dotenv').config(); // Import sensitive data from .env
+require('dotenv').config(); 
 
 const sequelize = process.env.JAWSDB_URL 
-  ? new Sequelize(process.env.JAWSDB_URL) // JAWSDB_URL is for HEROKU
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { // This is for Local
+  ? new Sequelize(process.env.JAWSDB_URL) 
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { 
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
