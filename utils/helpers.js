1292039997e7cf4bf00b581737460c401e1be288
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-04-30 13:09:49 -0500

    adding comments for better understanding

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,5 @@
+//-- Adding an object containing a single method called format_date--//
+//-- The method takes a date parameter and returns a formatted string in the format "MM/DD/YYYY"--//
 module.exports = {
   format_date: (date) => { // Format date information to MM/DD/YYYY
     return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
