a9ebb71a322be1b379e56aa097148e512719e4f2
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-05-02 18:24:29 -0500

    deploying heroku

--- config/connection.js
+++ config/connection.js
@@ -7,8 +7,8 @@
 const Sequelize = require('sequelize');
 require('dotenv').config(); 
 
-const sequelize = process.env.JAWSDB_URL 
-  ? new Sequelize(process.env.JAWSDB_URL) 
+const sequelize = process.env.CLEARDB_DATABASE_URL
+  ? new Sequelize(process.env.CLEARDB_DATABASE_URL) 
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { 
       host: 'localhost',
       dialect: 'mysql',
